ca0de0a6d1e962fbb8bd8fa981586fb99dfb4641
rashmikaofficial98@gmail.com <rashmikaofficial98@gmail.com>
2021-08-14 01:52:40 +0530

    Udemy ES6cTutorial-Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//const name ='Pasindu';
+
+
+function greetings(name){
+    return `Welcome ${name}` 
+};
+console.log(greetings('Pasindu'));
+
+//const newName = 'Hello ${name}';
+const mGreetings= (name)=>{
+    return `Hello ${name}`
+}
+console.log(mGreetings('Rashmika'));
