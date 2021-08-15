76032927ef0cef2f5e54ef7c9cd33c9251ff6e80
rashmikaofficial98@gmail.com <rashmikaofficial98@gmail.com>
2021-08-15 10:57:53 +0530

    Spread Operator

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 
-//const name ='Pasindu';
-
-
+    // Normal Function
 function greetings(name){
     return `Welcome ${name}` 
 };
 console.log(greetings('Pasindu'));
 
-//const newName = 'Hello ${name}';
+    // Array Function
 const mGreetings= (name)=>{
     return `Hello ${name}`
 }
 console.log(mGreetings('Rashmika'));
+
+    // Spread Operator
+
+let mArr1 = [1,2,3,4,5];
+let mArr2=[...mArr1];
+mArr1.push(6);
+
+console.log(mArr1);
+console.log(mArr2);
